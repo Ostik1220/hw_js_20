86ff5e88255b0ff2e717cc712352e4d81dacdb57
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-02-02 22:41:57 +0200

    зробив 2 завдання

--- place.js
+++ place.js
@@ -1,11 +1,68 @@
-const galleryElement = document.querySelector(".gallery")
-console.log(galleryElement)
-let x = 5
+const images = document.querySelectorAll(".image");
+const lightbox = document.getElementById("lightbox");
+const fullImg = document.getElementById("fullImg");
+let currentIndex = 0;
+
+images.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    fullImg.src = img.src;
+    lightbox.style.display = "flex";
+    currentIndex = index;
+  });
+});
+
 document.addEventListener("keydown", (e) => {
-galleryElement.style.transform = `translateX(${x}px)`
-if(e.code === 'ArrowRight'){
-    x += 5
-      } else if (e.code === 'ArrowLeft'){
-        x -= 5
-      }
+  if (lightbox.style.display === "flex") {
+    if (e.key === "ArrowRight") {
+      currentIndex = (currentIndex + 1) % images.length;
+    } else if (e.key === "ArrowLeft") {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+    }
+    fullImg.src = images[currentIndex].src;
+  }
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+//2
+const inputNumber = document.querySelector("input");
+const addButton = document.querySelector('[data-action="render"]');
+const delButton = document.querySelector('[data-action="destroy"]')
+let inputNum = 0
+inputNumber.addEventListener("blur", (e) => {
+  inputNum = Number(e.target.value); 
+  return console.log(inputNum)
 })
+
+
+let containre = document.querySelector("#boxes") 
+addButton.addEventListener("click", () => {
+  function createBoxes(amount){
+
+    for (let i = 0; i < amount; i++) {
+      let red = Math.round(Math.random() * (255 - 0) + 0);
+      let green = Math.round(Math.random() * (255 - 0) + 0);
+      let blue = Math.round(Math.random() * (255 - 0) + 0);
+      const box = document.createElement("div")
+      box.style.width = "30px"
+      box.style.height = "30px"
+      box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+      containre.append(box)
+    }
+    return console.log(containre)
+    }
+  
+    createBoxes(inputNum)
+})
+
+delButton.addEventListener("click", () => {
+  function destroyBoxes(){
+  containre.innerHTML = ""
+  return containre
+  }
+  destroyBoxes()
+})
+
+
